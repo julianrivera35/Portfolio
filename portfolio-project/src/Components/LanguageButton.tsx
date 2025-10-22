3346022fe37db63ba2ca48ced05384ecbd4d2496
Julian Rivera <julianrivera@Julians-MacBook-Air.local>
2025-10-22 02:30:50 -0500

    [UPDATE] Finished about me section

--- portfolio-project/src/Components/LanguageButton.tsx
+++ portfolio-project/src/Components/LanguageButton.tsx
@@ -6,8 +6,13 @@ const LanguageButton = () => {
         i18n.changeLanguage(lang);
     }
     return (
-        <button className="group relative border border-gray-300 bg-white text-slate-500 text-2xl px-2 py-1 rounded min-w-[150px] font-semibold">
-            {t("language.buttonLabel")}
+        <button className="group relative border border-gray-300 bg-white text-slate-500 text-2xl px-2 py-1 rounded xl:min-w-[150px] font-semibold">
+            <span className="hidden sm:inline">
+                {t("language.buttonLabel")}
+            </span>
+            <span className="sm:hidden">
+                {t("language.buttonLabelShort")}
+            </span>
             <div className="bg-stone-100 absolute top-full right-0 rounded-lg p-2 mt-1 min-w-[150px] shadow-md scale-y-0 group-focus:scale-y-100 origin-top duration-200 font-normal flex flex-col gap-3">
                 <a onClick={() => handleChangeLanguage("en")} className='hover:bg-stone-200/50 rounded-2xl py-1 px-2'> {t("language.english")} </a>
                 <a onClick={() => handleChangeLanguage("es")} className='hover:bg-stone-200/50 rounded-2xl py-1 px-2'> {t("language.spanish")} </a>
--- portfolio-project/src/Sections/AboutSection.tsx
+++ portfolio-project/src/Sections/AboutSection.tsx
@@ -1,12 +1,14 @@
 import { useTranslation } from 'react-i18next';
-
+import PhotoSeneca from '../assets/SenecaPhoto.webp'
 const AboutSection = () => {
     const t = useTranslation("global")[0];  
     return (
-        <div className='section-container secondary flex-col lg:flex-row items-start'>
-            <div className='flex-1'></div>
+        <div className='section-container secondary flex-col gap-2 lg:flex-row items-start'>
+            <div className=' flex flex-1 justify-center items-center'>
+                <img src={PhotoSeneca} alt="Julian's personal foto" className='max-h-[75vh]' />
+            </div>
             <div className='flex flex-col justify-center flex-1 gap-2 mr-10 ml-10'>
-                <h1 className='text-left mb-10'>{t("about.title")}</h1>
+                <h1 className='text-left mt-10 mb-5'>{t("about.title")}</h1>
                 <h2 className='pb-8 underline underline-offset-auto'>{t("about.subtitle")}</h2>
                 <p className='text-balance pb-2'>
                     {t("about.info")}
